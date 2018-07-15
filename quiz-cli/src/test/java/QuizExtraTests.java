e4903998c3793b60ee686c680a81f03f031bb69f
BranSeal <BranSeal@users.noreply.github.com>
2018-07-15 04:40:30 -0400

    - Added names for classes in task cards - Added placeholder test names

--- quiz-cli/src/test/java/QuizExtraTests.java
+++ quiz-cli/src/test/java/QuizExtraTests.java
@@ -1,5 +1,6 @@
 import edu.gatech.quiz.data.QuizDB;
 import org.junit.Before;
+import org.junit.Test;
 
 public class QuizExtraTests {
     QuizDB db;
@@ -7,5 +8,80 @@ public class QuizExtraTests {
     @Before
     public void setUp() {
         db = new QuizDB();
+    }
+
+
+    // Create multiple tests of the same feature/concept if testing
+    // different cases
+    // Example, testChooseCategory1 and testChooseCategory2
+
+    @Test
+    public void testTitleDisplay() {
+
+    }
+
+    @Test
+    public void testCategoryPrint() {
+
+    }
+
+    @Test
+    public void testChooseCategory() {
+
+    }
+
+    @Test
+    public void testQuestionOutput() {
+
+    }
+
+    @Test
+    public void testQuestionInput() {
+
+    }
+
+    @Test
+    public void testEndOfQuizScore() {
+
+    }
+
+    @Test
+    public void testDisplayEndOfQuizOptions() {
+
+    }
+
+    @Test
+    public void testEndOfQuizInput() {
+
+    }
+
+    @Test
+    public void testEndOfQuizToExplanation() {
+
+    }
+
+    @Test
+    public void testEndOfQuizToTakeNewQuiz() {
+
+    }
+
+    @Test
+    public void testEndOfQuizToDashboard() {
+
+    }
+
+    @Test
+    public void testEndOfQuizToQuit() {
+
+    }
+
+    @Test
+    public void testExplanationOutput() {
+
+    }
+
+    @Test
+    public void testDashBoardOutput() {
+
     }
 }
